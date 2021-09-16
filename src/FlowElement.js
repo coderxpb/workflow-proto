6b1f497c0065da38e7a2d1008463e56e42274cf1
coderxpb <borapankaj901@gmail.com>
2021-09-16 14:31:28 +0530

    Add toggle menu and custom email node

--- src/FlowElement.js
+++ src/FlowElement.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import ReactFlow from 'react-flow-renderer';
 import './FlowElement.scss';
-
+import EmailNode from './CustomNodes/EmailNode';
 const style = {height: '80vh', background:'#FFB'}
 
 const initialElements = [
-	{ id: '1', data: { label: '-' ,email: 'world'}, position: { x: 100, y: 100 } },
+	{ id: '1', type:'email',data: { label: '-', content:'asdfasdflj oh' ,email: 'world, okok, hello, alright, nvm,kkadsfkjlka'}, position: { x: 100, y: 100 } },
 	{ id: '2', data: { label: 'Node 3' }, position: { x: 100, y: 200 } },
 	{ id: 'e1-2', source: '1', target: '2' },
 ];
@@ -22,14 +22,17 @@ const FlowElement = () => {
 		setSelectedNode(element);
 	}
 
+	const onPaneClicked = (e, element) =>{
+		setSelectedNode(null);
+	}
+
+	//change element name
 	useEffect(() => {
 		console.log(selectedNode);
 		if(selectedNode!=null) {
 			setElements((els) =>
 				els.map((el) => {
 					if (el.id === selectedNode.id) {
-						// it's important that you create a new object here
-						// in order to notify react flow about the change
 						el.data = {
 							...el.data,
 							label: nodeName,
@@ -40,9 +43,8 @@ const FlowElement = () => {
 				})
 			);
 		}
-		
-	}, [nodeName, setElements]);
 
+	}, [nodeName, setElements]);
 
 
 	useEffect(() => {
@@ -59,6 +61,7 @@ const FlowElement = () => {
 		);
 	}, [nodeBg, setElements]);
 
+	//show hide slider
 	useEffect(() => {
 		if(selectedNode!=null) {
 			setSliderClassName("info-slider");
@@ -71,17 +74,13 @@ const FlowElement = () => {
 
 	return (
 		<React.Fragment>
-			<ReactFlow onElementClick={onNodeClicked} style={style} elements={elements} defaultZoom={1.5} minZoom={0.2} maxZoom={4}/>
+			<ReactFlow nodeTypes={{email: EmailNode}} onPaneClick={onPaneClicked} onElementClick={onNodeClicked} style={style} elements={elements} defaultZoom={1.5} minZoom={0.2} maxZoom={4}/>
+			
 			<div className={sliderClassName}>
 				<label>label:</label>
-				<input
-					value={nodeName}
-					onChange={(evt) => setNodeName(evt.target.value)}
-				/>
-
+				<input value={nodeName} onChange={(evt) => setNodeName(evt.target.value)}/>
 				<label className="updatenode__bglabel">background:</label>
 				<input value={nodeBg} onChange={(evt) => setNodeBg(evt.target.value)} />
-
 			</div>
 		</React.Fragment>
 	);
